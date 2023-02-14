ea79d1fc19940b30ee16e46b2edc4c256de8aeda
Ruan Leoterio <64181301+ruanleoterio@users.noreply.github.com>
2023-02-14 10:46:44 +0000

    implement highscore feature

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 'use strict';
 
-const ai = Math.trunc(Math.random()*20)+1
-console.log(ai)
-
-    let score = 20
+let ai = Math.trunc(Math.random()*20)+1
+let highscore = 0
+let score = 20
 
 document.querySelector('.check').addEventListener("click", () => {
     const guess = Number(document.querySelector('.guess').value)
-
+    // No input Number
     if(!guess){
         document.querySelector('.message').textContent = "⛔ no number ⛔"
     }
     else{
+        // when the guess is correct 
         if(guess === ai){
-            document.querySelector('body').style.backgroundColor = 'green'
+            document.querySelector('body').style.backgroundColor = '#60b347'
             document.querySelector('.message').textContent = "You Won"
             document.querySelector('.number').textContent = ai
+            document.querySelector('.highscore').textContent = highscore<score? score:highscore
+
+        // when guess is too low
         }else if(guess < ai){
-            if(score > 0){
-                document.querySelector('.message').textContent =  "it's almost, it's a little bigger"
+            if(score > 1){
+                document.querySelector('.message').textContent =  "Too low"
                 score--
                 document.querySelector('.score').textContent = score
             }else{
                 document.querySelector('body').style.backgroundColor = 'red'
                 document.querySelector('.message').textContent = 'You lost the game!'
             }
-
+        
+        // when guess is too high
         }else if(guess > ai){
-            if (score > 0 ){
-            document.querySelector('.message').textContent =  "it's almost, it's less"
+            if (score > 1){
+            document.querySelector('.message').textContent =  "Too high"
             score--
             document.querySelector('.score').textContent = score
             }else{
@@ -42,5 +46,13 @@ document.querySelector('.check').addEventListener("click", () => {
 
 
 document.querySelector('.btn.again').addEventListener('click',() =>{
-    location.reload()
+    score = 20
+    ai = Math.trunc(Math.random()*20)+1
+
+    document.querySelector('.guess').value = " "
+    document.querySelector('.message').textContent = "Start guessing..."
+    document.querySelector('.score').textContent = score
+    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.number').textContent = "?"
+
 })
